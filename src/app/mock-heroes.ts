4cfb727965aa5e1599563fa215f515a4c36ee880
Yong Cho Yin <me@techmastary.net>
2018-04-30 01:18:06 -0400

    4. Displaying a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: "Jordan" },
+  { id: 12, name: "Elaine" },
+  { id: 13, name: "Desmond"},
+  { id: 14, name: "Gwyneth"},
+  { id: 15, name: "Sherman"},
+  { id: 16, name: "Marco"},
+  { id: 17, name: "Arthur"},
+  { id: 18, name: "Paul"},
+  { id: 19, name: "Daniel"},
+  { id: 20, name: "Cassie"}
+  ];
